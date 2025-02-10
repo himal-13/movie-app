9fa33c8ad9057181eafa20334fee33759770575f
Himal bhusal <mememimi413@gmail.com>
2025-02-10 21:01:02 +0545

    genres page updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import TrendingPage from './pages/TrendingPage.tsx';
 import Favorite from './pages/Favorite.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import HistoryPage from './pages/HistoryPage.tsx';
 import Movie from './pages/Movie.tsx';
 import FavoriteProvider from './services/context.tsx';
 import RateReviewPage from './pages/RateReviewPage.tsx';
+import GenresPage from './pages/GenresPage.tsx';
 
 const router = createBrowserRouter([
   {
@@ -18,8 +18,8 @@ const router = createBrowserRouter([
     errorElement:<ErrorPage/>
   },
   {
-    path:'/trending',
-    element:<TrendingPage/>,
+    path:'/genres',
+    element:<GenresPage/>,
     errorElement:<ErrorPage/>
     
   },
